31987a88fe358ee38157281dce571924aa992c49
Wanderer6095 <167986650+Wanderer6095@users.noreply.github.com>
2025-04-05 22:18:43 +0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,20 @@
+from flask import Flask, url_for, request
+import os
+
+os.chdir(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+
+@app.route('/home', methods=['POST', 'GET'])
+def astronaut_selection():
+    if request.method == 'GET':
+        url_style = url_for('static', filename='css/style2.css')
+        with open("5.html", encoding="utf-8") as file:
+            html = file.read()
+        html = html.replace("{style}", url_style)
+        return html
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)

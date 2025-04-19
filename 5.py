84cf89e67090242a939a755f5c90107d01e75f8e
Wanderer6095 <167986650+Wanderer6095@users.noreply.github.com>
2025-04-19 16:40:48 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 
 @app.route('/home', methods=['POST', 'GET'])
 def astronaut_selection():
-    if request.method == 'GET':
-        url_style = url_for('static', filename='css/style2.css')
-        with open("5.html", encoding="utf-8") as file:
-            html = file.read()
-        html = html.replace("{style}", url_style)
-        return html
-    elif request.method == 'POST':
-        for key, value in request.form.items():
-            print(key, "-", value)
-        print(request.form.get('photo', ""))
-        return "Форма отправлена"
+    
+    if request.method == "POST":
+        file = (request.files.get("file"))
+        with open(url_for('static', filename='img/MARS-6.png')[1:], "wb") as out_file:
+            out_file.write(file.read())
+    url_style = url_for('static', filename='css/style2.css')
+    with open("5.html", encoding="utf-8") as file:
+        html = file.read()
+    html = html.replace("{style}", url_style)
+    return html
+
 
 
 if __name__ == '__main__':
